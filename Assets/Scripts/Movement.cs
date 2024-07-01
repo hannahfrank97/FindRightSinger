07052a4826255c4992ee487e0666fefe76376bc3
hannahhannah20 <cc221009@fhstp.ac.at>
2024-07-01 12:35:42 +0200

    Revert "Movement implementation"
    This reverts commit 06378c7590175fc3ae7ef32a70d1a0037b7b5c0b.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,45 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [Header ("Movement")]
-
-    public float movementSpeed;
-
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-
-    private void Start() {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
-    private void Update() {
-        MyInput();
-    }
-
-    private void FixedUpdate() {
-        MovePlayer();
-    }
-
-    private void MyInput() {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-    
-    }
-
-    private void MovePlayer() {
-
-      moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-      rb.AddForce(moveDirection.normalized * movementSpeed * 10f, ForceMode.Force);
-
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
