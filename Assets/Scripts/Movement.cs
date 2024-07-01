06378c7590175fc3ae7ef32a70d1a0037b7b5c0b
hannahhannah20 <cc221009@fhstp.ac.at>
2024-07-01 12:31:33 +0200

    Movement implementation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header ("Movement")]
+
+    public float movementSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        MyInput();
+    }
+
+    private void FixedUpdate() {
+        MovePlayer();
+    }
+
+    private void MyInput() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    
+    }
+
+    private void MovePlayer() {
+
+      moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+      rb.AddForce(moveDirection.normalized * movementSpeed * 10f, ForceMode.Force);
+
+
+
     }
 }
